ac3d4f19eff04e93fcefa75cbc8b32b39a3ff729
dcZhangTW <dczhang@thoughtworks.com>
2018-12-19 16:50:22 +0800

    Add component & spec

--- test/unit/specs/Tab.spec.js
+++ test/unit/specs/Tab.spec.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Tab from '@/components/Tab'
+import routes from '@/router'
+import VueRouter from 'vue-router'
+
+const mockRouter = new VueRouter(routes)
 
 describe('Tab', () => {
   it('should render correct contents', () => {
@@ -12,9 +16,10 @@ describe('Tab', () => {
             path: '/'
           }
         ]
-      }
+      },
+      router: mockRouter
     }).$mount()
-    expect(vm.$el.querySelector('li').length).to.equal(1)
-    expect(vm.$el.querySelector('li a span')[0].text).to.equal('Test')
+    expect(vm.$el.querySelectorAll('ul li').length).to.equal(1)
+    expect(vm.$el.querySelector('li a span').innerText).to.equal('Test')
   })
 })
