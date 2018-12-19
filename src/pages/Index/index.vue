948d2eb450f397c183473e10dd2f240da81cb365
dcZhangTW <dczhang@thoughtworks.com>
2018-12-19 14:05:56 +0800

    Add new page

---
+++ src/pages/Index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div> This is Index page</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: 'Test'
+    }
+  },
+  mounted () {
+    alert('Show Index Page')
+  }
+}
+</script>
